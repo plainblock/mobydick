6554185d1aba84587dfb8b7895e3f38697143a9f
plainblock <shohezaka.little@gmail.com>
2022-08-12 00:12:18 +0900

    Rename class

---
+++ java/src/main/java/io/github/plainblock/mobydick/Launcher.java
@@ -0,0 +1,9 @@
+package io.github.plainblock.mobydick;
+
+public class Launcher {
+
+    public static void main(String... args) {
+        MobyDick.main(args);
+    }
+
+}
--- java/src/main/java/io/github/plainblock/mobydick/MobyDick.java
+++ java/src/main/java/io/github/plainblock/mobydick/MobyDick.java
@@ -1,9 +1,33 @@
 package io.github.plainblock.mobydick;
 
-public class MobyDick {
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-    public static void main(String... args) {
-        MobyDickApplication.main(args);
+public class MobyDick extends Application {
+
+    private static final String TITLE = "MobyDick Java edition";
+    private static final String FXML = "moby-dick.fxml";
+    private static final String CSS = "moby-dick.css";
+    private static final int WIDTH = 640;
+    private static final int HEIGHT = 480;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader root = new FXMLLoader(MobyDick.class.getResource(FXML));
+
+        Scene scene = new Scene(root.load(), WIDTH, HEIGHT);
+        scene.getStylesheets().add(MobyDick.class.getResource(CSS).toExternalForm());
+
+        stage.setTitle(TITLE);
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
