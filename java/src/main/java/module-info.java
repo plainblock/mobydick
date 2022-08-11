eaf7463d86c5df6572bc06b795e5e7dfdd62e5ff
plainblock <shohezaka.little@gmail.com>
2022-08-11 16:08:34 +0900

    Edit dependencies

--- java/src/main/java/module-info.java
+++ java/src/main/java/module-info.java
@@ -1,9 +1,16 @@
-module io.github.plainblock.mobydick {
+module mobydick {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.databind;
     requires org.kordamp.bootstrapfx.core;
+    requires org.slf4j;
 
     opens io.github.plainblock.mobydick to javafx.fxml;
+    opens io.github.plainblock.mobydick.infrastructure.google.data to com.fasterxml.jackson.databind;
+
     exports io.github.plainblock.mobydick;
+    exports io.github.plainblock.mobydick.controller;
+    opens io.github.plainblock.mobydick.controller to javafx.fxml;
 }
