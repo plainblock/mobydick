898f832464c483da871086896948ffbb4eff1d1b
plainblock <shohezaka.little@gmail.com>
2022-08-14 02:10:38 +0900

    Edit: Extend BaseService

--- java/src/main/java/io/github/plainblock/mobydick/service/ManagementService.java
+++ java/src/main/java/io/github/plainblock/mobydick/service/ManagementService.java
@@ -1,19 +1,62 @@
 package io.github.plainblock.mobydick.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import io.github.plainblock.mobydick.domain.model.entity.Book;
-import io.github.plainblock.mobydick.domain.model.object.BookId;
+import io.github.plainblock.mobydick.domain.model.object.BookStatus;
+
 import io.github.plainblock.mobydick.domain.repository.InternalRepository;
 
-public class ManagementService {
+public class ManagementService extends BaseService {
 
     private final InternalRepository internal;
+
+    private List<Book> managedBooks = new ArrayList<>();
+    private String keywordCache = "";
 
     public ManagementService(InternalRepository internal) {
         this.internal = internal;
     }
 
-    public Book findById(String id) {
-        return internal.getBook(new BookId(id)).orElseThrow();
+    public List<Book> getManagedBooks() {
+        return managedBooks;
+    }
+
+    public String loadBooks(String keyword, BookStatus status) {
+        try {
+            keywordCache = keyword;
+            managedBooks = internal.searchBooks(keyword, status);
+            if (managedBooks.isEmpty()) {
+                return "条件にあう書籍が見つかりませんでした";
+            }
+            return String.format("%d件見つかりました", managedBooks.size());
+        } catch (Exception e) {
+            return formatErrorMessage(e);
+        }
+    }
+
+    public String registerBook(Book book, BookStatus status) {
+        try {
+            List<Book> registered = internal.filterBooks(book.getTitle(), book.getAuthor(), book.getPublisher(), book.getIsbn(), null);
+            if (registered.isEmpty()) {
+                book.create(internal, status);
+                return "本棚に登録しました";
+            }
+            return "既に本棚に登録済みです";
+        } catch (Exception e) {
+            return formatErrorMessage(e);
+        }
+    }
+
+    public String updateBookStatus(Book book, BookStatus status) {
+        try {
+            book.update(internal, status);
+            managedBooks = internal.searchBooks(keywordCache, status);
+            return "ステータスを更新しました";
+        } catch (Exception e) {
+            return formatErrorMessage(e);
+        }
     }
 
 }
--- java/src/main/java/io/github/plainblock/mobydick/service/ReferenceService.java
+++ java/src/main/java/io/github/plainblock/mobydick/service/ReferenceService.java
@@ -1,20 +1,35 @@
 package io.github.plainblock.mobydick.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import io.github.plainblock.mobydick.domain.model.entity.Book;
 import io.github.plainblock.mobydick.domain.repository.ExternalRepository;
 
-public class ReferenceService {
+public class ReferenceService extends BaseService {
 
     private final ExternalRepository external;
+
+    private List<Book> fetchedBooks = new ArrayList<>();
 
     public ReferenceService(ExternalRepository external) {
         this.external = external;
     }
 
-    public List<Book> findWithCondition(String title, String author, String publisher) {
-        return external.searchBooks(title, author, publisher);
+    public List<Book> getFetchedBooks() {
+        return fetchedBooks;
+    }
+
+    public String findWithCondition(String title, String author, String publisher) {
+        try {
+            fetchedBooks = external.searchBooks(title, author, publisher);
+            if (fetchedBooks.isEmpty()) {
+                return "条件にあう書籍が見つかりませんでした";
+            }
+            return String.format("%d件見つかりました", fetchedBooks.size());
+        } catch (Exception e) {
+            return formatErrorMessage(e);
+        }
     }
 
 }
