b4f7899721508751b9ae9e80b9f7d3e27b34827f
plainblock <shohezaka.little@gmail.com>
2022-08-14 14:48:10 +0900

    Implement: Add constructor

--- java/src/main/java/io/github/plainblock/mobydick/presentation/component/atom/CustomTextLabel.java
+++ java/src/main/java/io/github/plainblock/mobydick/presentation/component/atom/CustomTextLabel.java
@@ -1,17 +1,34 @@
 package io.github.plainblock.mobydick.presentation.component.atom;
 
 import java.awt.Font;
+import javax.swing.Icon;
 import javax.swing.JLabel;
 
 public class CustomTextLabel extends JLabel {
 
     public CustomTextLabel() {
-        this(null);
+        this("", null, LEADING);
     }
 
     public CustomTextLabel(String text) {
-        super(text);
+        this(text, null, LEADING);
+    }
+
+    public CustomTextLabel(String text, int horizontalAlignment) {
+        this(text, null, horizontalAlignment);
+    }
+
+    public CustomTextLabel(String text, Icon icon, int horizontalAlignment) {
+        super(text, icon, horizontalAlignment);
         setFont(new Font("Dialog", Font.BOLD, 14));
+    }
+
+    public CustomTextLabel(Icon image) {
+        this(null, image, CENTER);
+    }
+
+    public CustomTextLabel(Icon image, int horizontalAlignment) {
+        this(null, image, horizontalAlignment);
     }
 
 }
